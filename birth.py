cfbb859ef02d32031749f41a6b1316ad2af5fad5
m11233 <202303532@pua.edu.eg>
2025-08-21 01:10:21 +0300

    Update birth.py

--- birth.py
+++ birth.py
@@ -11,7 +11,7 @@ st.title("🎉 Happy Birthday, Bestie! 🎈")
 
 # Put images inside each column
 
-st.image(""\Screenshot (54).png"", caption="Birthday Fun 🎉",width=300)
+st.image(""Screenshot (54).png"", caption="Birthday Fun 🎉",width=300)
 
 
 
@@ -40,5 +40,6 @@ if st.button("Click here for a surprise! 🎁"):
 
 # 🕒 Footer
 st.caption(f"Sent on {date.today().strftime('%B %d, %Y')} 💌")
+
 
 
